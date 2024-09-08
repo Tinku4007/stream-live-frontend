db97da6e956bcf7a7729ecdd12b503bd60d9ea6e
Tinku4007 <tinkusaini1252@gmail.com>
2024-09-08 15:05:38 +0530

    code update

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -54,6 +54,8 @@ const Room = ({ roomId, isStreamer }) => {
         }
     }, [remoteStreams]);
 
+    console.log(remoteStreams , "remoteStreams")
+
     return (
         <div>
             {isStreamer && <video ref={localVideoRef} autoPlay playsInline muted style={{ width: '100%', height: 'auto' }} />}
